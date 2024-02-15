59d65b5d973ac6d0759cde8aa50921b7af882c56
Khaled Javedan <khaledjavedan76@gmail.com>
2024-02-15 19:21:41 +0400

    chore: added changelog

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-    jsxInject: `import { h, Fragment } from 'vue';`
-  },
+
   plugins: [vue()],
 });
